8388ae3aa1344c94564a8be0bb231d8cbb63faa6
Glitch (hello-express) <none>
2020-11-07 15:48:29 +0000

    📻🚦 Checkpoint ./komutlar/jail.js:23964415/968 ./komutlar/mute.js:23964415/4

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -1,42 +1,48 @@
 const Discord = require('discord.js');
 const db = require("quick.db")
 exports.run = async (client, message, args) => {
- if (!message.member.roles.has('768796888971345921')) return message.channel.sendEmbed(new Discord.RichEmbed().setColor("RED").setDescription('Bu komutu kullanabilmek için \`Commanders\` yetkisine sahip olmalısın!') );
+ let hata = new Discord.MessageEmbed()
+ .setDescription('<a:basarisiz:757851005483221022> **Bu komudu kullanabilmek için** <@&774353380611981312> **yetkisine sahip olmalısın!**')
+ .setColor('RED')
+ 
+if (!message.member.roles.cache.get("774353380611981312")) return message.channel.send(hata) 
+  
   let kullanıcı = message.mentions.users.first()
-  if (!kullanıcı) return message.channel.sendEmbed(new Discord.RichEmbed().setColor("RED").setDescription('Bir üye etiketlemen gerekiyor!'));
+  if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription('Bir üye etiketlemen gerekiyor!'));
   let user = message.mentions.users.first();
   let rol = message.mentions.roles.first()
   let member = message.guild.member(kullanıcı)
    let reason = args.slice(1).join(" ")
-      if(!reason) return message.channel.send(new Discord.RichEmbed().setColor("RED").setDescription("Jaile atmak için sebep belirtmelisin!")).then(m => m.delete(5000));
+      if(!reason) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription("Jaile atmak için sebep belirtmelisin!")).then(m => m.delete(5000));
   message.guild.members.get(member.id).roles.forEach(r => {
 message.guild.members.get(member.id).removeRole(r) //DCS EKİPİ
 
    
 })
-  member.addRole('752513177236930571')
-     const kanal = message.guild.channels.find(c => c.id == "768797576848867339") // DCS EKİPİ
-    const embed1 = new Discord.RichEmbed() 
+  member.roles.add('752513177236930571')
+const embed1 = new Discord.MessageEmbed()
     .setAuthor('Bir Üye Cezalıya Atıldı')
     .addField(`Jaile Atılan Kullanıcı`,` ${kullanıcı}`)
 .addField(`Jaile Atan Yetkili`,` <@${message.author.id}>`)
   .addField(`Jaile Atılma Sebebi`, `${reason} `)
-                          .setThumbnail(message.author.avatarURL)
+                          .setThumbnail(message.author.avatarURL())
 
     .setColor("GREEN")
- .setFooter(`${message.author.tag} tarafından komut kullanılmıştır..`, message.author.avatarURL)
+ .setFooter(`${message.author.tag} tarafından komut kullanılmıştır..`, message.author.avatarURL())
     .setTimestamp()
+    
+client.channels.cache.get('774349626081148958').send(embed)
   // DCS EKİPİ
   
-  let embed = new Discord.RichEmbed() 
+  let embed = new Discord.MessageEmbed() 
   .setDescription(`${kullanıcı} Adlı Kişisinin Tüm Rolleri Alınarak, <@&752513177236930571> Rolü Verildi! `) 
-                      .setThumbnail(message.author.avatarURL)
+                      .setThumbnail(message.author.avatarURL())
 
- .setFooter(`${message.author.tag} tarafından komut kullanılmıştır..`, message.author.avatarURL)
+ .setFooter(`${message.author.tag} tarafından komut kullanılmıştır..`, message.author.avatarURL())
   .setTimestamp()
       .setColor("GREEN")
 
-  return message.channel.send(embed).then(kanal.send(embed1)).then(m => m.delete(5000000));
+  return message.channel.send(embed).then(m => m.delete(5000000));
   
 }
 
--- komutlar/mute.js
+++ komutlar/mute.js
@@ -1,3 +1,7 @@
+
+
+
+
 const Discord = require("discord.js");
 const ms = require("ms");
 const ayarlar = require("../ayarlar.json");
