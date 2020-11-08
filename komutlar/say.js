220b6b59cf1dbc33cd375ba65699118cf9dfdf1a
Glitch (hello-express) <none>
2020-11-08 07:00:27 +0000

    💂🎷 Checkpoint ./komutlar/say.js:23964415/1000 ./komutlar/emojiler.js:23964415/1105

--- komutlar/say.js
+++ komutlar/say.js
@@ -6,18 +6,15 @@ exports.run = async (client, message, args) => {
      let botlar = message.guild.members.cache.filter(m => m.user.bot).size;
       var toplamEtiketliUyeler = message.guild.members.cache.filter(member => member.user.username.includes("")).size
 
+      
     let textChannels = message.guild.channels.cache.filter(m => m.type == "text").size;
     for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
   let  dark_kod = message.guild.members.cache.filter(m => !m.user.bot && m.user.presence.status !== "offline").size 
     const darkcode  = new Discord.MessageEmbed() 
         .setColor("RED")
         .addField(`<a:sagok:757855573554233396> **Sunucudaki Toplam Üye Sayısı**`,`» **${message.guild.memberCount}**`)
-        .addField(`<a:sagok:757855573554233396> **Toplam Çevrimiçi Üye Sayısı**`, `» **${dark_kod}**`) 
         .addField(`<a:sagok:757855573554233396> **Seslideki Üye Sayısı**`,`» **${count}**`) 
-        .addField(`<a:sagok:757855573554233396> **Yazı Kanalları**`, ` » **${textChannels}**`)
-        .addField(`Taglı Üye Sayısı.`, `» **${toplamEtiketliUyeler}**`) 
-        .addField(`<a:sagok:757855573554233396> **Roller**`,`»  **${message.guild.roles.cache.size}**`)
-        .addField(`<a:sagok:757855573554233396> **Emojiler**`,`»  **${message.guild.emojis.cache.size}**`) 
+        .addField(`<a:sagok:757855573554233396> **Taglı Alan Üye Sayısı.**`, `» **${toplamEtiketliUyeler}**`) 
 .setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `)
              .setThumbnail( message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 
