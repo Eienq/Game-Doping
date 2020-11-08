ce782725f4009a5e98dc7ffbf41cb002e3c969ba
Glitch (hello-express) <none>
2020-11-08 06:14:27 +0000

    🕹🏩 Checkpoint ./komutlar/jail.js:23964415/366 ./komutlar/mute.js:23964415/2505

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -1,22 +1,22 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
- let hata = new Discord.MessageEmbed()
+ 
+  let hata = new Discord.MessageEmbed()
  .setDescription('<a:basarisiz:757851005483221022> **Bu komudu kullanabilmek için** <@&774353377974157353> **yetkisine sahip olmalısın!**')
  .setColor('RED')
  
 if (!message.member.roles.cache.get("774353377974157353")) return message.channel.send(hata) 
-  
-  let kullanıcı = message.mentions.users.first()
-  if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription('Bir üye etiketlemen gerekiyor!'));
-  let user = message.mentions.users.first();
-  let rol = message.mentions.roles.first()
-  let member = message.guild.member(kullanıcı)
-   let reason = args.slice(1).join(" ")
-      if(!reason) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription("Jaile atmak için sebep belirtmelisin!"));
+let kullanıcı = message.mentions.users.first()
+if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription('Bir üye etiketlemen gerekiyor!'));
+let user = message.mentions.users.first();
+let rol = message.mentions.roles.first()
+let member = message.guild.member(kullanıcı)
+let reason = args.slice(1).join(" ")
+if(!reason) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription("Jaile atmak için sebep belirtmelisin!"));
   
 member.roles.cache.forEach(r => {
-  member.roles.add('774353402858700832');
-  member.roles.remove(r.id)
+member.roles.add('774353402858700832');
+member.roles.remove(r.id)
 
    
 })
@@ -27,15 +27,13 @@ const embed1 = new Discord.MessageEmbed()
 .addField(`Jaile Atılma Sebebi`, `${reason} `)
 .setColor("BLACK")
 .setThumbnail( message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-.setFooter(`${message.author.tag} tarafından komut kullanılmıştır..`, message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+.setFooter(`Geliştirici qmi <3`, message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 .setTimestamp()
 client.channels.cache.get('774349665700675615').send(embed1)
   
   let embed = new Discord.MessageEmbed() 
 .setDescription(`${kullanıcı} Adlı Kişisinin Tüm Rolleri Alınarak, <@&774353402858700832> Rolü Verildi! `) 
-.setTimestamp()
 .setColor("BLACK")
-.setFooter(`${message.author.tag} tarafından komut kullanılmıştır..`, message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 return message.channel.send(embed);
   
 }
--- komutlar/mute.js
+++ komutlar/mute.js
@@ -5,15 +5,11 @@
 const Discord = require("discord.js");
 const ms = require("ms");
 const ayarlar = require("../ayarlar.json");
-const db = require('quick.db');
 
 const prefix = ayarlar.prefix;
 
 
 module.exports.run = async (bot, message, args, member) => {
-  var muterol = db.fetch(`muterol_${message.guild.id}`)
-  if (!muterol) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Sunucuda Mute Rolü Ayarlanmadığı İçin Komut Kullanılamaz! Not: Mute Rolü Ayarlamak İçin z!mrol @rol `))
-
 
    let hata = new Discord.MessageEmbed()
  .setDescription('<a:basarisiz:757851005483221022> **Bu komudu kullanabilmek için** <@&774353380611981312> **yetkisine sahip olmalısın!**')
@@ -30,40 +26,31 @@ if (!message.member.roles.cache.get("774353380611981312")) return message.channe
   if (mutekisi.hasPermission("MANAGE_MESSAGES"))
     return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Yetkili bir kişiyi muteleyemem!`));
   let sebep = args.splice(2, args.length).join(" ");
-  if (!muterol) {
-    try {
-      muterol = await message.guild.roles.create({
-      });
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.createOverwrite(muterol, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false
-        });
-      });
-    } catch (e) {
-      console.log(e.stack);
-    }
-  }
   let mutezaman = args[1]
 
   if (!mutezaman) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:basarisiz:757851005483221022> Lütfen bir zaman giriniz! \n 1 Saniye = 1s \n 1 Dakika = 1m \n 1 Saat = 1h \n 1 Gün = 1d`));
 
-await mutekisi.roles.add(muterol);
-  message.channel.send(
-    new Discord.MessageEmbed()
-      .setThumbnail( message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-    .setColor('GREEN')
+await mutekisi.roles.add('774353404855451658');
+const embed1 = new Discord.MessageEmbed()
+    .setColor('BLACK')
     .setAuthor("Bir Kullanıcı Susturuldu")
-    .setTimestamp()
     .addField("**Kullanıcı:**", `<@${mutekisi.id}>`)
     .addField("**Yetkili:**", message.author)
     .addField("**Süre:**", args[1])
     .addField("**Sebep:**", `${sebep === "" ? "Sebep belirtilmemiş." : sebep}`)
-  );
+    .setThumbnail( message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+    .setTimestamp()
+bot.channels.cache.get('774349665700675615').send(embed1)
+
+ 
+
+                      
+                      ;
+  
 
   setTimeout(function() {
-  mutekisi.roles.remove(muterol)
-    message.channel.send(new Discord.MessageEmbed().setColor('GREEN').setTimestamp().setDescription(`<@${mutekisi.id}> kullanıcısının mutelenme süresi sona erdi!`)
+  mutekisi.roles.remove('774353404855451658')
+    message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTimestamp().setDescription(`<@${mutekisi.id}> kullanıcısının mutelenme süresi sona erdi!`)
 );
   }, ms(mutezaman));
 };
