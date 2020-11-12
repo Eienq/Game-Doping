69d5a03d74d8616968e89d51859c9007a7b62c4a
Glitch (gifted-fine-guarantee) <none>
2020-11-12 14:33:04 +0000

    🗾⚽️ Checkpoint ./komutlar/unmute.js:14888641/218

--- komutlar/unmute.js
+++ komutlar/unmute.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
  
-  let hata = new Discord.MessageEmbed()
- .setDescription('<a:basarisiz:757851005483221022> **Bu komudu kullanabilmek için** <@&770868327748599808> **yetkisine sahip olmalısın!**')
+  let yetkiyok = new Discord.MessageEmbed()
+ .setDescription('<a:basarisiz:757851005483221022> **Bu komudu kullanabilmek için** <@&YETKİLİ ROL İD> **yetkisine sahip olmalısın!**')
  .setColor('RED')
  
-if (!message.member.roles.cache.get("770868327748599808")) return message.channel.send(hata) 
+if (!message.member.roles.cache.get("YETKİLİ ROL İD")) return message.channel.send(yetkiyok) //Yetkili İd
 let kullanıcı = message.mentions.users.first()
 if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription('Bir üye etiketlemen gerekiyor!'));
 let user = message.mentions.users.first();
 let rol = message.mentions.roles.first()
 let member = message.guild.member(kullanıcı)
-member.roles.remove('775801882752188426')
+member.roles.remove('MUTE ROL İD')//ALINACAK ROL
 
    
 
@@ -23,10 +23,10 @@ const embed1 = new Discord.MessageEmbed()
 .setThumbnail( message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 .setFooter(`Geliştirici qmi <3`, message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 .setTimestamp()
-client.channels.cache.get('775358713723748372').send(embed1)
+client.channels.cache.get('LOG KANAL İD').send(embed1)//KANAL İD
   
   let embed = new Discord.MessageEmbed() 
-.setDescription(`${kullanıcı} Adlı Kişisinin <@&775801882752188426> Rolü Alınarak, susturulması kaldırıldı `) 
+.setDescription(`${kullanıcı} Adlı Kişisinin <@&MUTE ROL İD> Rolü Alınarak, susturulması kaldırıldı `) 
 .setColor("#92dffe")
 return message.channel.send(embed);
   
