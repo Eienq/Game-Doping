3b8654a799e4adc055c881456f27821971f1669b
Glitch (gifted-fine-guarantee) <none>
2020-11-16 05:59:11 +0000

    🎷👐 Checkpoint ./komutlar/git.js:14888641/2268 ./events/ready.js:14888641/186

---
+++ komutlar/git.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const data = require('quick.db');
+const ayarlar = require("../ayarlar.json");
+
+
+exports.run = async (client, message, args) => {
+let prefix = ayarlar.prefix;
+if(!message.member.permissions.has('MOVE_MEMBERS')) return message.channel.send(new Discord.MessageEmbed()
+.setDescription(`**Bu Komudu Kullanabilmek İçin, \`Üyeleri Taşı\` Yetkisine Sahip Olmalısın.**`));
+
+if(message.member.voice.channel == undefined) return message.channel.send(new Discord.MessageEmbed()
+.setDescription('Bir Sesli Kanalda Değilsin!'))
+if(!message.mentions.members.first()) return message.channel.send(new Discord.MessageEmbed()
+.setDescription('Bir Kullanıcı Etiketle!'));
+
+if(message.author.id === message.mentions.members.first()) return;
+if(message.mentions.members.first().voice.channel == undefined) return message.channel.send(new Discord.MessageEmbed()
+.setDescription(`Etiketlediğin ${message.mentions.members.first()} Sesli kanalda yok.`))
+message.guild.members.cache.get(message.author.id).voice.setChannel(message.mentions.members.first().voice.channel.id);
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [""],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'git',
+  description: 'Seslide ki Bir Üyenin Yanına Gidersiniz.',
+  usage: 'git'
+};
