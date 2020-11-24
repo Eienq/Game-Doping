a5fe143c7b4f9344bb294fcdfaf183b1c617a3e3
Glitch (gifted-fine-guarantee) <none>
2020-11-24 10:08:21 +0000

    🛍🍴 Checkpoint ./komutlar/ban.js:12037333/211:14888641/5526

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,41 +1,43 @@
 const Discord = require('discord.js');
+const db = require('quick.db');
 const ayarlar = require("../ayarlar.json");
 
 
 exports.run = async(client, message, args) => {
   
-        let yetkili = ayarlar.banyetkili
-        let banlogkanal = ayarlar.banlog
-
-
-
-   let acebots = new Discord.MessageEmbed()
- .setDescription(`**Bu komudu kullanabilmek için** <@&${yetkili}>  **yetkisine sahip olmalısın!**`).setColor('#ff0000')
- if (!message.member.roles.cache.get(yetkili)) return message.channel.send(acebots) //acebots  
-
-  
-	   let user = message.mentions.users.first()
+    let acebot = new Discord.MessageEmbed().setColor('#70ff70').setFooter(`Komut ${message.author.tag} Tarafından Kullanıldı ! `).setTimestamp();
+     let yetkili = ayarlar.jailyetkili
+  if (!message.member.hasPermission("BAN_ADD")) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDesciption(` Bu komutu kullanabilmek için "\`yönetici\`" yetkisine sahip olmalısın`));
+  let user = message.mentions.users.first()
+    
     let sebep = args.slice(1).join(' ') || "Belirtilmemiş."
-     if(!user) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:basarisiz:757851005483221022> Bir kişi etiketlemelisin.'))
-     if(user.id === message.author.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:basarisiz:757851005483221022> Kendini banlayamazsın.'))
-     if(user.id === client.user.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:basarisiz:757851005483221022> Botu banlayamazsın.'))
-     if(user.id === message.guild.ownerID) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:basarisiz:757851005483221022> Sunucu sahibini banlayamazsın.'))
-     if (!message.guild.member(user).bannable) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:basarisiz:757851005483221022> Bu kişinin rolü senden üstte veya `Üyeleri yasakla` yetkisine sahip.'));
+    if(!user) return message.channel.send(acebot.setDescription(`Bir kişi etiketlemelisin.`))
+    if(user.id === message.author.id) return message.channel.send(acebot.setDescription('Kendini banlayamazsın.'))
+    if(user.id === client.user.id) return message.channel.send(acebot.setDescription('Botu banlayamazsın.'))
+    if(user.id === message.guild.ownerID) return message.channel.send(acebot.setDescription ('Sunucu sahibini banlayamazsın.'))
+    if (!message.guild.member(user).bannable) return message.channel.send(acebot.setDescription(' Bu kişinin rolü senden üstte veya `Üyeleri yasakla` yetkisine sahip.'));
 
+   message.channel.send(acebot.setDescription('<@'+ user.id + '> Kişisini **'+ sebep+ '** Sebebiyle banlamak istediğine eminmisin ?')).then(async m => {
+   	 m.react('✅').then(r =>{ const tamam = (reaction,user) => reaction.emoji.name == '✅' && user.id == message.author.id;
+      const tamam2 = m.createReactionCollector(tamam)
 
-   client.on('collect', async (r)=>{
-  message.guild.members.cache.get(user.id).ban({reason: `${sebep}`})
-      let embed = new Discord.MessageEmbed()
-    .setColor('GREEN')
-    .setTitle('Kişi banlandı')
-    .addField('Yetkili', `${message.author.tag}`)
-    .addField('Banlanan kişi', user)
-    .addField('Sebep', sebep)
-client.channels.cache.get(banlogkanal).send(embed)//Log Kanalı
+   tamam2.on('collect', async (r)=>{message.guild.members.cache.get(user.id).ban({reason: `${sebep}`})
+      let embed = acebot.setDescription(`${user} adlı kullanıcı ${message.author.id} tarafından \`${sebep}\` sebebi ile banlandı. `)
+    message.channel.send(embed)
        })
-    }
- 
- 
+    })
+    await m.react('❌').then(r =>{ 
+
+   const tamam = (reaction,user) => reaction.emoji.name == '❌' && user.id == message.author.id;
+      const tamam2 = m.createReactionCollector(tamam)
+
+   tamam2.on('collect', async (r)=>{
+     m.delete()
+message.channel.send(acebot.setDescription('Banlama işlemi iptal edildi.'))
+      })()
+    })
+ })
+} 
  
 exports.conf = {
 	enabled: true,
